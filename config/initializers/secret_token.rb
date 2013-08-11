e8b87ab6d03915702e56e956547ad7130160022c
Lucas Zeer-Wanklyn <lucas.zeer@gmail.com>
2013-08-11 19:24:24 +0200

    The beginning
    details of the app so far :
    Using Rails 4.0, Ruby 2.0, & mysql database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MemoryMocha::Application.config.secret_key_base = 'abd7715b8676aabca17efd602758f04086ade1080bd9d41a5debf6a13307cafa09768539cd039f1e4fec36f42b5355ad29400a048e38e56d7f18be8711765f82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MemoryMocha::Application.config.session_store :cookie_store, key: '_MemoryMocha_session'
